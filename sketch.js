c6daea8e8ec88c97478a05db714e8575240b7ced
anupriya2005 <69389390+anupriya2005@users.noreply.github.com>
2020-09-02 16:40:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,91 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var box1, box2,box3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	var packageSprite_options={
+		isStatic:true
+	}
+
+	box1.createSprite(400,350,200,20);
+	box2.createSprite(390,340,20,100);
+	box3.createSprite(410,360,20,100);
+
+	var box1_options={
+		isStatic:true
+	}
+	var box2_options={
+		isStatic:true
+	}
+	var box3_options={
+		isStatic:true
+	}
+	World.add(box1, box2, box3);
+	 
+	box1.shapeColor="red";
+	box2.shapeColor="red";
+	box3.shapeColor="red";
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+
+	console.log(object);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	packageBody.collide(ground);
+	Matter.Body.setStatic(packageSprite, false);
+	var packageBody= {
+		restitution: 1
+	}
+  }
+}
